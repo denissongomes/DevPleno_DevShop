7af23526c9b37f4e472ff20900713200a7524921
Denisson Gomes <dnsgom@gmail.com>
2020-10-30 20:00:32 -0300

    Set view engine to ejs

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const express = require('express')
 const app =  express ()
 const port = process.env.PORT || 3000
 
-app.get('/', (req,res) => {
+app.set('view engine', 'ejs')
+
+app.get('/', (req, res) => {
 
     res.send('DevShop Test')
 
