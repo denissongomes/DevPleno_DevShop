00d5d7857ba57e467794875c29417b361b941f1b
Denisson Gomes <dnsgom@gmail.com>
2020-10-30 19:13:10 -0300

    Send test response  to "/"

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ const express = require('express')
 const app =  express ()
 const port = process.env.PORT || 3000
 
+app.get('/', (req,res) => {
+
+    res.send('DevShop Test')
+
+})
 
 app.listen(port, (err) => {
     if(err){
