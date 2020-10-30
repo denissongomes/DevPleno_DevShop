fb5a1ab2bda44119d41fe6387170dea481fac37b
Denisson Gomes <dnsgom@gmail.com>
2020-10-30 19:07:54 -0300

    Bind and listen for connections on the host/port

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 const express = require('express')
 const app =  express ()
 const port = process.env.PORT || 3000
+
+
+app.listen(port, (err) => {
+    if(err){
+        console.log('Não foi possível iniciar o servidor')
+    } else {
+        console.log('DevShop server online...') 
+    }    
+})
