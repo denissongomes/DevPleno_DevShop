fa3d7405f25b84f1fb3539df74597fa0d55a04a1
Denisson Gomes <dnsgom@gmail.com>
2020-10-31 12:05:28 -0300

    Remove unnecessary piece of code

--- controllers/categories.js
+++ controllers/categories.js
@@ -1,5 +1,4 @@
 const getCategories = db => async(req, res) => {
-    const categories = await category.getCategories(db)()
     const products = await product.getProductsByCategoryId(db)(req.params.id)
     const cat = await category.getCategoriesById(db)(req.params.id)
     res.render('category', {
