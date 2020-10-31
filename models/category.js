b496e0362c3149e69e1030df84945bf3557b10b8
Denisson Gomes <dnsgom@gmail.com>
2020-10-31 08:30:49 -0300

    Create a model category refatored from index.js

---
+++ models/category.js
@@ -0,0 +1,16 @@
+
+const getCategoriesById = async(id) => {
+    const category = await db('categories')
+                            .select('*')
+                            .where('id', id)
+    return category
+}
+
+const getCategories = async() => {
+    const categories = await db('categories').select('*')
+    const categoriesWithSlug = categories.map( category => {
+    const newCategory = { ...category, slug: slug(category.category) }
+    return newCategory
+})
+return categoriesWithSlug
+}
