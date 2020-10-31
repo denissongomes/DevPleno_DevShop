df22c2e99b5611d3b93d49b4562bd11078175886
Denisson Gomes <dnsgom@gmail.com>
2020-10-31 08:54:34 -0300

    Inject db dependency and export functions

--- models/category.js
+++ models/category.js
@@ -1,16 +1,21 @@
 
-const getCategoriesById = async(id) => {
+const getCategoriesById = db => async(id) => {
     const category = await db('categories')
                             .select('*')
                             .where('id', id)
     return category
 }
 
-const getCategories = async() => {
+const getCategories = db => async() => {
     const categories = await db('categories').select('*')
     const categoriesWithSlug = categories.map( category => {
     const newCategory = { ...category, slug: slug(category.category) }
     return newCategory
 })
 return categoriesWithSlug
+}
+
+module.exports = {
+    getCategories,
+    getCategoriesById
 }
