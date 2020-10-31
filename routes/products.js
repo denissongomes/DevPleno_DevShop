bc1251437c785f7cc11663d45bd47d4abbf66216
Denisson Gomes <dnsgom@gmail.com>
2020-10-31 17:45:27 -0300

    Move to route index

--- routes/products.js
+++ routes/products.js
@@ -1,7 +1,7 @@
 const init = db => {
     const router = require('express').Router()
     const products = require('../controllers/products')
-    router.get('/produto/:id/:slug', products.getProduct(db))
+    router.get('/:id/:slug', products.getProduct(db))
     return router
 }
 
