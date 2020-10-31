df47d6caa60a284cc94a7dc92b041aeff0a6249d
Denisson Gomes <dnsgom@gmail.com>
2020-10-31 18:06:37 -0300

    Convert into function to inject db dependency

--- app.js
+++ app.js
@@ -1,20 +1,22 @@
-const express = require('express')
-const app =  express ()
+const init = db => {
+    const express = require('express')
+    const app =  express ()
 
-const category = require('./models/category')
-const routes = require('./routes')
+    const category = require('./models/category')
+    const routes = require('./routes')
 
-app.set('view engine', 'ejs')
-app.use(express.static('public'))
+    app.set('view engine', 'ejs')
+    app.use(express.static('public'))
 
-//middleware
-app.use(async(req, res, next) => {
-    const categories = await category.getCategories(db)()
-    res.locals = {
-        categories
-    }
-    next()
-})
+    //middleware
+    app.use(async(req, res, next) => {
+        const categories = await category.getCategories(db)()
+        res.locals = {
+            categories
+        }
+        next()
+    })
+    app.use(routes(db))
+}
 
-
-app.use(routes(db))
+module.exports = init
