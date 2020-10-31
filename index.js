f4d58e8ba5f5a50456e4b3b3fd92619d34947efa
Denisson Gomes <dnsgom@gmail.com>
2020-10-31 17:59:08 -0300

    Move to app.js

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
-const express = require('express')
-const app =  express ()
-const port = process.env.PORT || 3000
-const category = require('./models/category')
 
-const routes = require('./routes')
+const port = process.env.PORT || 3000
+
 
 const db = require('knex')({
     client: 'mysql2',
@@ -19,20 +16,8 @@ db.on('query', query => {
     console.log('SQL: ', query.sql)
 })
 
-app.set('view engine', 'ejs')
-app.use(express.static('public'))
-
-//middleware
-app.use(async(req, res, next) => {
-    const categories = await category.getCategories(db)()
-    res.locals = {
-        categories
-    }
-    next()
-})
 
 
-app.use(routes(db))
 
 app.listen(port, (err) => {
     if(err){
