11f000d52b610318c8dab3ea8462eeff0b42d5d4
Denisson Gomes <dnsgom@gmail.com>
2020-10-31 12:06:54 -0300

    Import required models

--- controllers/categories.js
+++ controllers/categories.js
@@ -1,3 +1,6 @@
+const category = require('../models/category.js')
+const product = require('../models/product.js')
+
 const getCategories = db => async(req, res) => {
     const products = await product.getProductsByCategoryId(db)(req.params.id)
     const cat = await category.getCategoriesById(db)(req.params.id)
