07fa3cbc04bb4af5d598d6a7d32582c253d82fb6
Denisson Gomes <dnsgom@gmail.com>
2020-11-01 20:41:52 -0300

    Create admin categories

---
+++ routes/admin/categories.js
@@ -0,0 +1,8 @@
+const init = db => {
+    const router = require('express').Router()
+    const categories = require('../../controllers/categories')
+    router.get('/', categories.getCategories(db))
+    return router
+}
+
+module.exports = init
