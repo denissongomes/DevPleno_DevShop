aa1360e3d08d7f9c928d875de9ec8612c4e2e14d
Denisson Gomes <dnsgom@gmail.com>
2020-11-01 15:51:53 -0300

    Add router for login

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,13 @@
 const init = db => {
     const home = require('../controllers/home')
+    const auth = require('../controllers/auth')
 
     const categories = require('./categories')
     const products = require('./products')
     
     const router = require('express').Router()
     router.get('/', home.getIndex)
+    router.post('/login', auth.login(db))
     router.use('/categoria', categories(db))
     router.use('/produto',products(db))
    
