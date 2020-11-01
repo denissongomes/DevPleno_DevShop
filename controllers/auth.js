dc456a6b36c75146255244bd0d8b80e27d691d11
Denisson Gomes <dnsgom@gmail.com>
2020-11-01 17:07:00 -0300

    If user athenticated, define user session

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,6 +2,7 @@ const user = require('../models/user')
 const login = db => async(req, res) => {
     try {
         const userFromDb = await user.login(db)(req.body.email, req.body.passwd) 
+        req.session.user = userFromDb
         res.send(userFromDb)
     }catch(err){
         res.send('Error:' + err)
