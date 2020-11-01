563df52c73d6eb47ab2d2c6b778b604a9abb5326
Denisson Gomes <dnsgom@gmail.com>
2020-11-01 16:54:01 -0300

    Catch error if user not exists

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,7 +1,12 @@
 const user = require('../models/user')
 const login = db => async(req, res) => {
-    const userFromDb = await user.login(db)(req.body.email, req.body.passwd) 
-    res.send(userFromDb)
+    try {
+        const userFromDb = await user.login(db)(req.body.email, req.body.passwd) 
+        res.send(userFromDb)
+    }catch(err){
+        res.send('Error:' + err)
+    }
+
 }
 
 module.exports = {
