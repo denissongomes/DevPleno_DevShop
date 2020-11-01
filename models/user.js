a537792f8cfa3249c96b4c2a20d44a8c5ff87c8a
Denisson Gomes <dnsgom@gmail.com>
2020-11-01 15:02:54 -0300

    Add initialUser() to create admin if not exists

---
+++ models/user.js
@@ -0,0 +1,23 @@
+const initialUser =  db => async(id) => {
+    //check if there is no users
+    const count = await db('users').count('id as total')
+    if(count[0].total===0){
+        //create admin
+        const user = {
+            name: 'Admin',
+            email: 'admin@devshop.com',
+            passwd: 'pass',
+            email_checked: true,
+            roles: 'admin',
+            created: new Date(),
+            updated: new Date()
+        }
+      await db('users').insert(user)  
+    }
+                
+     
+}
+
+module.exports = {
+    initialUser
+}
