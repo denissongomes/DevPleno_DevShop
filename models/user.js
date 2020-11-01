1628f68ad7e1d8644c08778ec1c7cb46aa13f9f6
Denisson Gomes <dnsgom@gmail.com>
2020-11-01 16:53:08 -0300

    Check if user exists or pass error to controller

--- models/user.js
+++ models/user.js
@@ -28,6 +28,9 @@ const initialUser =  db => async(id) => {
 
 const login = db => async(email, passwd) => {
     const user =  await db('users').select('*').where('email',email)
+   if (user.length === 0){
+       throw new Error('Invalid user.')
+   }
     return user
 
 }
