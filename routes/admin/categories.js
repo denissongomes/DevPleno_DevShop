20b7e68253caae2ecef9e1b16320916fa53c6379
Denisson Gomes <dnsgom@gmail.com>
2020-11-02 06:59:30 -0300

    Update router admin to create new category

--- routes/admin/categories.js
+++ routes/admin/categories.js
@@ -2,8 +2,8 @@ const init = db => {
     const router = require('express').Router()
     const categories = require('../../controllers/categories')
     router.get('/', categories.adminGetCategories(db))
-    router.get('/nova', categories.adminNewCategory(db))
-    router.post('/nova', categories.adminNewCategory(db))
+    router.get('/nova', categories.adminCreateCategory(db))
+    router.post('/nova', categories.adminCreateCategory(db))
 
     return router
 }
