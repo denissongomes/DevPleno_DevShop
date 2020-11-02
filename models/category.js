6c810d97412c35f26a9c716c0d4137b4fc547d8b
Denisson Gomes <dnsgom@gmail.com>
2020-11-02 08:26:38 -0300

    Move function to utils/validation.js

--- models/category.js
+++ models/category.js
@@ -22,19 +22,6 @@ const getCategories = db => async() => {
 return categoriesWithSlug
 }
 
-const extractErrors = error => {
-    return error.details.reduce((prev, curr) => {
-        if(prev[curr.path[0]]){
-            prev[curr.path[0]].push(curr.type)
-        } else {
-            prev[curr.path[0]] = [curr.type]
-        }
-        return prev
-    }, {})     
-    
-
-}
-
 const createCategory = db => async(category) => {
    const { err, value } = Joy.validate(category, createSchema, { abortEarly: false, stripUnknown: true })
    if(error){
